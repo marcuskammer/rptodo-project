b6d43958ed0b2a3d567b7d541dd235b9d2812ee4
Marcus Kammer <2262664-marcus-kammer@users.noreply.gitlab.com>
2022-06-10 13:51:39 +0200

    Create config files and database

--- rptodo/config.py
+++ rptodo/config.py
@@ -18,10 +18,35 @@ def init_app(db_path: str) -> int:
     if config_code != SUCCESS:
         return config_code
 
+    database_code = _create_database(db_path)
+    if database_code != SUCCESS:
+        return database_code
+
+    return SUCCESS
+
 
 def _init_config_file() -> int:
-    pass
+    try:
+        CONFIG_DIR_PATH.mkdir(exist_ok=True)
+    except OSError:
+        return DIR_ERROR
+
+    try:
+        CONFIG_FILE_PATH.touch(exist_ok=True)
+    except OSError:
+        return FILE_ERROR
+
+    return SUCCESS
 
 
 def _create_database(db_path: str) -> int:
-    pass
+    config_parser = configparser.ConfigParser()
+    config_parser['General'] = {'database': db_path}
+
+    try:
+        with CONFIG_FILE_PATH.open('w') as file:
+            config_parser.write(file)
+    except OSError:
+        return DB_WRITE_ERROR
+
+    return SUCCESS
