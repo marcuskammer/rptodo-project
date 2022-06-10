9921111533e1703f383e1341699d1e3488c222a4
Marcus Kammer <marcus.kammer@mailbox.org>
2022-06-10 13:27:32 +0200

    Create config functions

--- rptodo/config.py
+++ rptodo/config.py
@@ -11,3 +11,17 @@ from rptodo import (
 
 CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
 CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
+
+
+def init_app(db_path: str) -> int:
+    config_code = _init_config_file()
+    if config_code != SUCCESS:
+        return config_code
+
+
+def _init_config_file() -> int:
+    pass
+
+
+def _create_database(db_path: str) -> int:
+    pass
